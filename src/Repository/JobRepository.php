5a51589e907d7750cfa34df7a41180d1f1db6984
NguyenHuuYen <huuyen911@gmail.com>
2019-06-10 10:24:20 +0700

    day 8: add where query

--- src/Repository/JobRepository.php
+++ src/Repository/JobRepository.php
@@ -4,21 +4,39 @@
 namespace App\Repository;
 
 use App\Entity\Category;
+use App\Entity\Job;
 use Doctrine\ORM\AbstractQuery;
 use Doctrine\ORM\EntityRepository;
 
 class JobRepository extends EntityRepository
 {
-    // Check job is actived or not, if not yet return null
-    public function findActiveJob(int $id)
+    public function findActiveJobs(int $categoryId = null)
     {
-        return $this->createQueryBuilder('j')
-                    ->where('j.id = :id')
-                    ->andWhere('j.expiresAt > :date')
-                    ->setParameter('id', $id)
-                    ->setParameter('date', new \DateTime())
-                    ->getQuery()
-                    ->getOneOrNullResult();
+        $qb = $this->createQueryBuilder('j')
+                   ->where('j.expiresAt > :date')
+                   ->andWhere('j.activated = :activated')
+                   ->setParameter('date', new \DateTime())
+                   ->setParameter('activated', true)
+                   ->orderBy('j.expiresAt', 'DESC');
+        if ($categoryId) {
+            $qb->andWhere('j.category = :categoryId')
+               ->setParameter('categoryId', $categoryId);
+        }
+        return $qb->getQuery()->getResult();
+    }
+
+    // Check job is actived or not, if not yet return null
+    public function findActiveJob(int $id): ?Job
+    {
+         return $this->createQueryBuilder('j')
+                     ->where('j.id = :id')
+                     ->andWhere('j.expiresAt > :date')
+                     ->andWhere('j.activated = :activated')
+                     ->setParameter('id', $id)
+                     ->setParameter('date', new \DateTime())
+                     ->setParameter('activated', true)
+                     ->getQuery()
+                     ->getOneOrNullResult();
     }
 
     // Get active jobs in category
@@ -27,8 +45,10 @@ class JobRepository extends EntityRepository
         return $this->createQueryBuilder('j')
                     ->where('j.category = :category')
                     ->andWhere('j.expiresAt > :date')
+                    ->andWhere('j.activated = :activated')
                     ->setParameter('category', $category)
                     ->setParameter('date', new \DateTime())
+                    ->setParameter('activated', true)
                     ->getQuery();
     }
 }
