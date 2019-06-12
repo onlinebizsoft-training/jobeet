b065b7bee11865632f6916119f329fad1397810d
NguyenHuuYen <huuyen911@gmail.com>
2019-06-12 16:41:16 +0700

    day 12: crud affiliate admin

--- src/Controller/Admin/AffiliateController.php
+++ src/Controller/Admin/AffiliateController.php
@@ -8,7 +8,6 @@ use App\Entity\Affiliate;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
